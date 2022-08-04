ceabcf6070b16191c006c8416be550b28d15b3a8
hotien2107 <hotien2107@gmail.com>
2022-08-04 15:15:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
+const LayoutAdminNavbar = React.lazy(() => import("Layout/AdminNavbar"));
+const LayoutHeaderStats = React.lazy(() => import("Layout/HeaderStats"));
+const LayoutAdminDashboard = React.lazy(() => import("Layout/AdminDashboard"));
+const LayoutAdminSettings = React.lazy(() => import("Layout/AdminSettings"));
+const LayoutAdminTables = React.lazy(() => import("Layout/AdminTables"));
+const LayoutFooterAdmin = React.lazy(() => import("Layout/FooterAdmin"));
+const Sidebar = React.lazy(() => import("Sidebar/Index"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ErrorBoundary>
+        <Sidebar />
+      </ErrorBoundary>
+
+      <div className="relative md:ml-64 bg-blueGray-100">
+        <ErrorBoundary>
+          <LayoutAdminNavbar />
+        </ErrorBoundary>
+        {/* Header */}
+        <ErrorBoundary>
+          <LayoutHeaderStats />
+        </ErrorBoundary>
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          <Routes>
+            <Route
+              path="/"
+              exact
+              element={
+                <ErrorBoundary>
+                  <LayoutAdminDashboard />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="/settings"
+              exact
+              element={
+                <ErrorBoundary>
+                  <LayoutAdminSettings />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="/tables"
+              exact
+              element={
+                <ErrorBoundary>
+                  <LayoutAdminTables />
+                </ErrorBoundary>
+              }
+            />
+          </Routes>
+
+          <ErrorBoundary>
+            <LayoutFooterAdmin />
+          </ErrorBoundary>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { dependencies } = require("./package.json");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "Main",
+      remotes: {
+        Sidebar: `Sidebar@http://localhost:3001/moduleEntry.js`,
+        Layout: `Layout@http://localhost:3002/moduleEntry.js`,
+      },
+      shared: {
+        ...dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+        "react-router-dom": {
+          singleton: true,
+          requiredVersion: dependencies["react-router-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+};
